adfc34409fd8e93369ea1322074759f66e0777dd
salahuddinhussein <eng.salah.fadhil@gmail.com>
2018-10-31 22:20:16 +0300

    initial commit

---
+++ app/src/main/java/com/example/salahuddinhussein/ipcalcu/PagerAdapter.java
@@ -0,0 +1,34 @@
+package com.example.salahuddinhussein.ipcalcu;
+
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+class PagerAdapter extends FragmentStatePagerAdapter {
+    private final int mNumOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                return new EquivalentValues();
+            case 1:
+                return new VlsmAllocation();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
